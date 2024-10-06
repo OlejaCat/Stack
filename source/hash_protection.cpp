457739ea175b3a867a6c83817ca624a18961abe5
Oleg Katyshkin <olegkatyskin@gmail.com>
2024-10-06 20:22:06 +0300

    Style changes

--- source/hash_protection.cpp
+++ source/hash_protection.cpp
@@ -12,6 +12,7 @@ void initHash(void);
 
 // public --------------------------------------------------------------------------------------------------------------
 
+
 hash_type calculateHash(const void* data, const size_t size) {
     hash_type hash = 0;
     const uint8_t* bytes = (const uint8_t*)data;
@@ -22,6 +23,7 @@ hash_type calculateHash(const void* data, const size_t size) {
 
     return hash;
 }
+
 
 HashProtectionState checkHash(const hash_type hash_given, const void* data, const size_t size)
 {
